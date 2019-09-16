bf315a5bf7c1d42472cf98e53a82c52d007e9e0a
Melisse Cabral <melissecabral>
2019-09-16 15:34:09 -0300

    Add loader

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 function sendMensagem(msg) {
     let url = 'https://api.whatsapp.com/send?phone=5585999616120&text=' + msg
 }
+var java = function(){
+    document.querySelector(".java").className = "php1";
+};
+var css = function(){
+    document.querySelector(".css").className = "css1";
+};
+var htm = function(){
+    document.querySelector(".htm").className = "htm1";
+};
+var jav = function(){
+    document.querySelector(".javascript").className = "javascript1";
+};
+setTimeout(php,1000);
+setTimeout(css,3000);
+setTimeout(htm,5000);
+setTimeout(jav,7000);
